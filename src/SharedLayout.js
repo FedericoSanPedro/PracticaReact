4e8a408521036cf891601f3cec2159f48791f469
Federico <fedu_zero@hotmail.com>
2022-05-08 18:04:07 -0300

    StyledNavbar

--- src/SharedLayout.js
+++ src/SharedLayout.js
@@ -1,10 +1,11 @@
 import {Link,Outlet} from 'react-router-dom';
 import Navbar from '../component/Navbar';
+import StyledNavbar from '../component/StyledNavbar';
 
 const Home = () => {
     return (
     <>
-        <Navbar/>
+        <StyledNavbar/>
         <Outlet/>
     </>
     );
